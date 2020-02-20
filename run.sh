c97540a1ac26d3b6984da69dd55a2a72d992260c
lushun_mac <470651748@qq.com>
2020-02-20 10:33:28 +0800

    log_mbv2_1.0_0

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-python  -u  train.py -e mbv2_1.0_0 --gpu 7 \
---epochs 100 --weight_decay 0.0001 --learning_rate 0.1 --batch_size 256 \
---val_interval 5 > log_mobilenetv2 2>&1 &
+python  -u  train.py -e mbv2_1.0_0 --gpu 7 --data_dir /home/work/dataset/ILSVRC2012 \
+--epochs 150 --weight_decay 0.00004 --learning_rate 0.05 --batch_size 256 \
+--val_interval 5 > log_mbv2_1.0_0 2>&1 &
