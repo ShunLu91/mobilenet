28369f0ed22823c5202b7cb9ccf55da038e1567b
lushun_mac <470651748@qq.com>
2020-03-18 22:26:52 +0800

    data_transform

--- utils.py
+++ utils.py
@@ -86,14 +86,14 @@ def data_transforms(args):
             transforms.RandomResizedCrop(224),
             transforms.RandomHorizontalFlip(),
             # transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.2),
-            # transforms.ToTensor(),
-            # transforms.Normalize(MEAN, STD)
+            transforms.ToTensor(),
+            transforms.Normalize(MEAN, STD)
         ])
         valid_transform = transforms.Compose([
             transforms.Resize(256),
             transforms.CenterCrop(224),
-            # transforms.ToTensor(),
-            # transforms.Normalize(MEAN, STD)
+            transforms.ToTensor(),
+            transforms.Normalize(MEAN, STD)
         ])
 
     if args.cutout:
